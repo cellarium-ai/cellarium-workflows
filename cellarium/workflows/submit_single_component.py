f5a980ff74f962e251553250aeb66c89c87428bc
Stephen Fleming <sjfleming@users.noreply.github.com>
2024-11-20 01:43:51 -0500

    Remove unnecessary bit

--- cellarium/workflows/submit_single_component.py
+++ cellarium/workflows/submit_single_component.py
@@ -199,7 +199,7 @@ def submit_single_component_pipeline(
 
         # re-install cellarium-ml if a git sha is provided
         if git_sha != "":
-            cmd = f"yes | pip install -U git+https://github.com/cellarium-ai/cellarium-ml.git@{git_sha}"
+            cmd = f"pip install -U git+https://github.com/cellarium-ai/cellarium-ml.git@{git_sha}"
             os.system(cmd)
 
         # handle multi-node training
