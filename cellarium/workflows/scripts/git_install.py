52e59590799e74f6675dfc4f71aa2422f51e432e
Stephen Fleming <sjfleming@users.noreply.github.com>
2025-08-03 18:24:01 -0400

    install git to enable use of pytorch docker image

--- cellarium/workflows/scripts/git_install.py
+++ cellarium/workflows/scripts/git_install.py
@@ -3,5 +3,8 @@ import os
 
 # re-install cellarium-ml if a git sha is provided
 if git_sha != "":
+    os.system('apt-get update')
+    os.system('apt-get install -y git')
     cmd = f"pip install -U git+https://github.com/cellarium-ai/cellarium-ml.git@{git_sha}"
+    print(cmd)
     os.system(cmd)
