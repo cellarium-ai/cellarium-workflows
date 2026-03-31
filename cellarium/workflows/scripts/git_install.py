41ac151035014632f21a93007197f5cb4c61d490
Stephen Fleming <sjfleming@users.noreply.github.com>
2026-03-31 00:52:51 -0400

    speed up pip install

--- cellarium/workflows/scripts/git_install.py
+++ cellarium/workflows/scripts/git_install.py
@@ -3,9 +3,15 @@
 import os
 
 # re-install cellarium-ml if a git sha is provided
+# if git_sha != "":  # noqa: F821
+#     os.system("apt-get update")
+#     os.system("apt-get install -y git")
+#     cmd = f"pip install -U -q git+https://github.com/cellarium-ai/cellarium-ml.git@{git_sha}"  # noqa: F821
+#     print(cmd)
+#     os.system(cmd)
+
+# faster
 if git_sha != "":  # noqa: F821
-    os.system("apt-get update")
-    os.system("apt-get install -y git")
-    cmd = f"pip install -U -q git+https://github.com/cellarium-ai/cellarium-ml.git@{git_sha}"  # noqa: F821
+    cmd = f"pip install -U -q https://github.com/cellarium-ai/cellarium-ml/archive/{git_sha}.tar.gz"  # noqa: F821
     print(cmd)
     os.system(cmd)
